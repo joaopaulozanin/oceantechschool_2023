6684ed3550bdd5ff4d61e081a4608a06c1dea522
joaopaulozanin <joao.zanin@outlook.com.br>
2023-09-25 23:35:05 -0300

    :wrench: Estilização CSS e key correction .map

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,30 @@ import './App.css'
 import Card from './components/Card/Card'
 
 function App() {
-  const item1 = {
+  const personagem1 = {
+    id: 0,
     nome: 'Rick Sanches',
     imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
   }
 
-  const item2 = {
+  const personagem2 = {
+    id: 1,
     nome: 'Morty Smith',
     imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
   }
 
-  const item3 = {
+  const personagem3 = {
+    id: 2,
     nome: 'Summer Smith',
     imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
   }
 
-  const itens = [item1, item2, item3]
+  const itens = [personagem1, personagem2, personagem3]
 
   return (
     <>
-      {itens.map(function (item) {
-        return <Card item={item} />
+      {itens.map(function (personagem) {
+      return <Card key={personagem.id} item={personagem} />
       })}
     </>
   )
